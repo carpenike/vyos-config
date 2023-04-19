8d6896b858b818216f4dd1bf99ee2b495fcde482
Ryan Holt <ryan@ryanholt.net>
2023-04-19 13:36:02 -0400

    update containers to match current config

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -1,26 +1,35 @@
 #!/bin/vbash
 
 # Container networks
-set container network services prefix '10.5.0.0/24'
+set container network services prefix '10.6.0.0/16'
+
+# adguard-home
+set container name adguard-home image 'docker.io/adguard/adguardhome:v0.107.12'
+set container name adguard-home memory '0'
+set container name adguard-home network services address '10.6.0.6'
+set container name adguard-home restart 'on-failure'
+set container name adguard-home shared-memory '0'
+set container name adguard-home volume config destination '/opt/adguardhome/conf'
+set container name adguard-home volume data source '/config/adguard-home/conf'
 
 # cloudflare-ddns
-set container name cloudflare-ddns allow-host-networks
-set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
-set container name cloudflare-ddns environment DOMAINS value 'ipv4.schorgers.nl,ipv4.bjw-s.tech,ipv4.bjw-s.dev,ipv4.bjws.nl'
-set container name cloudflare-ddns environment IP6_PROVIDER value "none"
-set container name cloudflare-ddns environment TZ value 'Europe/Amsterdam'
-set container name cloudflare-ddns environment PGID value "1000"
-set container name cloudflare-ddns environment PUID value "1000"
-set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.9.2'
-set container name cloudflare-ddns memory '0'
-set container name cloudflare-ddns restart 'on-failure'
-set container name cloudflare-ddns shared-memory '0'
+# set container name cloudflare-ddns allow-host-networks
+# set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
+# set container name cloudflare-ddns environment DOMAINS value 'ipv4.schorgers.nl,ipv4.bjw-s.tech,ipv4.bjw-s.dev,ipv4.bjws.nl'
+# set container name cloudflare-ddns environment IP6_PROVIDER value "none"
+# set container name cloudflare-ddns environment TZ value 'Europe/Amsterdam'
+# set container name cloudflare-ddns environment PGID value "1000"
+# set container name cloudflare-ddns environment PUID value "1000"
+# set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.9.2'
+# set container name cloudflare-ddns memory '0'
+# set container name cloudflare-ddns restart 'on-failure'
+# set container name cloudflare-ddns shared-memory '0'
 
 # coredns - main instance
 set container name coredns cap-add 'net-bind-service'
 set container name coredns image 'docker.io/coredns/coredns:1.10.1'
 set container name coredns memory '0'
-set container name coredns network services address '10.5.0.3'
+set container name coredns network services address '10.6.0.3'
 set container name coredns restart 'on-failure'
 set container name coredns shared-memory '0'
 set container name coredns volume config destination '/config'
@@ -35,11 +44,11 @@ set container name coredns volume vyoshosts mode 'ro'
 
 # dnsdist
 set container name dnsdist cap-add 'net-bind-service'
-set container name dnsdist environment TZ value 'Europe/Amsterdam'
+set container name dnsdist environment TZ value 'America/New_York'
 set container name dnsdist image 'docker.io/powerdns/dnsdist-18:1.8.0'
 set container name dnsdist arguments '--log-timestamps'
 set container name dnsdist memory '0'
-set container name dnsdist network services address '10.5.0.4'
+set container name dnsdist network services address '10.6.0.4'
 set container name dnsdist restart 'on-failure'
 set container name dnsdist shared-memory '0'
 set container name dnsdist volume config destination '/etc/dnsdist/dnsdist.conf'
@@ -49,7 +58,7 @@ set container name dnsdist volume config mode 'ro'
 # haproxy-k8s-api
 set container name haproxy-k8s-api image 'docker.io/library/haproxy:2.7.6'
 set container name haproxy-k8s-api memory '0'
-set container name haproxy-k8s-api network services address '10.5.0.2'
+set container name haproxy-k8s-api network services address '10.6.0.2'
 set container name haproxy-k8s-api restart 'on-failure'
 set container name haproxy-k8s-api shared-memory '0'
 set container name haproxy-k8s-api volume config destination '/usr/local/etc/haproxy/haproxy.cfg'
@@ -85,7 +94,7 @@ set container name speedtest-exporter shared-memory '0'
 # udp-broadcast-relay-mdns
 set container name udp-broadcast-relay-mdns allow-host-networks
 set container name udp-broadcast-relay-mdns cap-add 'net-raw'
-set container name udp-broadcast-relay-mdns environment CFG_DEV value 'eth1.20;eth1.40'
+set container name udp-broadcast-relay-mdns environment CFG_DEV value 'bond0.20;bond0.40'
 set container name udp-broadcast-relay-mdns environment CFG_ID value '2'
 set container name udp-broadcast-relay-mdns environment CFG_MULTICAST value '224.0.0.251'
 set container name udp-broadcast-relay-mdns environment CFG_PORT value '5353'
@@ -98,7 +107,7 @@ set container name udp-broadcast-relay-mdns shared-memory '0'
 # udp-broadcast-relay-sonos
 set container name udp-broadcast-relay-sonos allow-host-networks
 set container name udp-broadcast-relay-sonos cap-add 'net-raw'
-set container name udp-broadcast-relay-sonos environment CFG_DEV value 'eth1.20;eth1.40'
+set container name udp-broadcast-relay-sonos environment CFG_DEV value 'bond0.20;bond0.40'
 set container name udp-broadcast-relay-sonos environment CFG_ID value '1'
 set container name udp-broadcast-relay-sonos environment CFG_MULTICAST value '239.255.255.250'
 set container name udp-broadcast-relay-sonos environment CFG_PORT value '1900'
@@ -107,17 +116,3 @@ set container name udp-broadcast-relay-sonos image 'ghcr.io/onedr0p/udp-broadcas
 set container name udp-broadcast-relay-sonos memory '0'
 set container name udp-broadcast-relay-sonos restart 'on-failure'
 set container name udp-broadcast-relay-sonos shared-memory '0'
-
-# unifi
-set container name unifi environment RUNAS_UID0 value 'false'
-set container name unifi environment TZ value 'Europe/Amsterdam'
-set container name unifi environment UNIFI_GID value '999'
-set container name unifi environment UNIFI_STDOUT value 'true'
-set container name unifi environment UNIFI_UID value '999'
-set container name unifi image 'ghcr.io/jacobalberty/unifi-docker:v7.3.83'
-set container name unifi memory '0'
-set container name unifi network services address '10.5.0.10'
-set container name unifi restart 'on-failure'
-set container name unifi shared-memory '0'
-set container name unifi volume data destination '/unifi'
-set container name unifi volume data source '/config/containers/unifi'
