a67f7f5481a79d34fb1616ac4a50e5d65ebdf2e2
Ryan Holt <ryan@ryanholt.net>
2023-04-19 10:41:36 -0400

    initial commit -- needs to be customized

---
+++ config-parts/interfaces.sh
@@ -0,0 +1,41 @@
+#!/bin/vbash
+
+# set interfaces ethernet eth0 address 'dhcp'
+# set interfaces ethernet eth0 description 'WAN'
+set interfaces ethernet eth0 hw-id 'e4:3a:6e:5f:33:9c'
+
+set interfaces ethernet eth1 address '10.1.0.1/24'
+set interfaces ethernet eth1 description 'LAN'
+set interfaces ethernet eth1 hw-id 'e4:3a:6e:5f:33:9d'
+set interfaces ethernet eth1 vif 10 address '10.1.1.1/24'
+set interfaces ethernet eth1 vif 10 description 'SERVERS'
+set interfaces ethernet eth1 vif 20 address '10.1.2.1/24'
+set interfaces ethernet eth1 vif 20 description 'TRUSTED'
+set interfaces ethernet eth1 vif 30 address '192.168.2.1/24'
+set interfaces ethernet eth1 vif 30 description 'GUEST'
+set interfaces ethernet eth1 vif 40 address '10.1.3.1/24'
+set interfaces ethernet eth1 vif 40 description 'IOT'
+set interfaces ethernet eth1 vif 50 address '10.1.4.1/24'
+set interfaces ethernet eth1 vif 50 description 'VIDEO'
+
+set interfaces ethernet eth2 hw-id 'e4:3a:6e:5f:33:9e'
+set interfaces ethernet eth3 hw-id 'e4:3a:6e:5f:33:9f'
+set interfaces ethernet eth4 hw-id 'e4:3a:6e:5f:33:a0'
+
+set interfaces ethernet eth5 hw-id 'e4:3a:6e:5f:33:a1'
+set interfaces ethernet eth5 description 'WAN'
+set interfaces ethernet eth5 address 'dhcp'
+
+set interfaces wireguard wg01 address '10.0.11.1/24'
+set interfaces wireguard wg01 description 'WIREGUARD'
+set interfaces wireguard wg01 peer ipad-bernd allowed-ips '10.0.11.4/32'
+set interfaces wireguard wg01 peer ipad-bernd persistent-keepalive '15'
+set interfaces wireguard wg01 peer ipad-bernd public-key 'EYuUoDoCIcSYbnjNyPSsZUcz4Yp032AHWflYlH2V1EM='
+set interfaces wireguard wg01 peer iphone-bernd allowed-ips '10.0.11.2/32'
+set interfaces wireguard wg01 peer iphone-bernd persistent-keepalive '15'
+set interfaces wireguard wg01 peer iphone-bernd public-key '608IT2YRPTCabLMGnpYfIWhgDnEpLV5IIpYi5J7m3As='
+set interfaces wireguard wg01 peer macbook-bernd allowed-ips '10.0.11.3/32'
+set interfaces wireguard wg01 peer macbook-bernd persistent-keepalive '15'
+set interfaces wireguard wg01 peer macbook-bernd public-key 'iWKTc88fDBhqlsAgoCXYvkBdf9wG4YBZ8XTg/+0Ln2I='
+set interfaces wireguard wg01 port '51820'
+set interfaces wireguard wg01 private-key "${SECRET_WIREGUARD_PRIVATE_KEY}"
