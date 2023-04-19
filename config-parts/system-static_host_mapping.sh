b90dede1e12bb90490f7c941802394b7239c7a97
Ryan Holt <ryan@ryanholt.net>
2023-04-19 13:52:03 -0400

    match config

--- config-parts/system-static_host_mapping.sh
+++ config-parts/system-static_host_mapping.sh
@@ -1,33 +1,24 @@
-# Gateway
-set system static-host-mapping host-name gateway.bjw-s.tech inet 10.1.0.1
-set system static-host-mapping host-name gateway.bjw-s.tech alias vpn.bjw-s.dev
-set system static-host-mapping host-name gateway.bjw-s.tech alias ipv4.bjw-s.dev
+#!/bin/vbash
 
-# Unifi controller
-set system static-host-mapping host-name unifi inet 10.5.0.10
+# Gateway
+set system static-host-mapping host-name fw.holthome.net inet 10.20.0.1
+set system static-host-mapping host-name fw.holthome.net alias vpn.holthome.net
+set system static-host-mapping host-name fw.holthome.net alias ipv4.holthome.net
 
 # NAS
-set system static-host-mapping host-name librarium.bjw-s.tech inet 10.1.1.11
-set system static-host-mapping host-name librarium.bjw-s.tech alias nas.bjw-s.tech
-set system static-host-mapping host-name librarium.bjw-s.tech alias minio.bjw-s.dev
-set system static-host-mapping host-name librarium.bjw-s.tech alias s3.bjw-s.dev
+set system static-host-mapping host-name nas-0.holthome.net inet 10.20.0.10
+set system static-host-mapping host-name nas-0.holthome.net alias nas.holthome.net
+set system static-host-mapping host-name nas-1.holthome.net inet 10.20.0.11
 
 # Kubernetes hosts
-set system static-host-mapping host-name delta.bjw-s.tech inet 10.1.1.31
-set system static-host-mapping host-name enigma.bjw-s.tech inet 10.1.1.32
-set system static-host-mapping host-name felix.bjw-s.tech inet 10.1.1.33
+set system static-host-mapping host-name cp-0.holthome.net inet 10.20.0.20
+set system static-host-mapping host-name node-0.holthome.net inet 10.20.10.20
+set system static-host-mapping host-name node-1.holthome.net inet 10.20.10.21
+set system static-host-mapping host-name node-2.holthome.net inet 10.20.10.22
+set system static-host-mapping host-name node-3.holthome.net inet 10.20.10.23
 
 # Kubernetes cluster VIP
-set system static-host-mapping host-name cluster-0.bjw-s.tech inet 10.5.0.2
+set system static-host-mapping host-name cluster-0.holthome.net inet 10.6.0.2
 
 # Other hosts
-set system static-host-mapping host-name diego.bjw-s.tech inet 10.1.1.52
-set system static-host-mapping host-name diego.bjw-s.tech alias pikvm.bjw-s.tech
-set system static-host-mapping host-name horus.bjw-s.tech inet 10.1.1.51
-
-set system static-host-mapping host-name driveway-camera-doorbell.bjw-s.tech inet 10.1.4.12
-set system static-host-mapping host-name hallway-zigbee-adapter.bjw-s.tech inet 10.1.3.46
-set system static-host-mapping host-name garage-tablet.bjw-s.tech inet 10.1.3.54
-set system static-host-mapping host-name hallway-tablet.bjw-s.tech inet 10.1.3.53
-set system static-host-mapping host-name livingroom-vacuum.bjw-s.tech inet 10.1.3.18
-set system static-host-mapping host-name upstairs-vacuum.bjw-s.tech inet 10.1.3.22
+set system static-host-mapping host-name adguard.holthome.net inet 10.6.0.6
