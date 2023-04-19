2a0a1a2bf2dfa71d9df9e22970e47071442a3a65
Ryan Holt <ryan@ryanholt.net>
2023-04-19 11:31:34 -0400

    update router protocols to match config

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -1,14 +1,20 @@
 #!/bin/vbash
 
 # BGP configuration
-set protocols bgp neighbor 10.1.1.31 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.31 description 'delta'
-set protocols bgp neighbor 10.1.1.31 remote-as '64512'
-set protocols bgp neighbor 10.1.1.32 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.32 description 'enigma'
-set protocols bgp neighbor 10.1.1.32 remote-as '64512'
-set protocols bgp neighbor 10.1.1.33 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.33 description 'felix'
-set protocols bgp neighbor 10.1.1.33 remote-as '64512'
-set protocols bgp parameters router-id '10.1.0.1'
+set protocols bgp neighbor 10.20.0.20 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.0.20 description 'cp-0'
+set protocols bgp neighbor 10.20.1.20 remote-as '64512'
+set protocols bgp neighbor 10.20.0.21 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.0.21 description 'node-0'
+set protocols bgp neighbor 10.20.1.21 remote-as '64512'
+set protocols bgp neighbor 10.20.0.22 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.0.22 description 'node-1'
+set protocols bgp neighbor 10.20.1.22 remote-as '64512'
+set protocols bgp neighbor 10.20.0.23 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.0.23 description 'node-2'
+set protocols bgp neighbor 10.20.1.23 remote-as '64512'
+set protocols bgp neighbor 10.20.0.24 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.0.24 description 'node-3'
+set protocols bgp neighbor 10.20.1.24 remote-as '64512'
+set protocols bgp parameters router-id '10.20.0.1'
 set protocols bgp system-as '64512'
