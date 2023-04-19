ad2879b52ec6fd4d284125ea52c6e490cfe80451
Ryan Holt <ryan@ryanholt.net>
2023-04-19 11:13:04 -0400

    update interfaces to match config on device

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -1,41 +1,60 @@
 #!/bin/vbash
 
-# set interfaces ethernet eth0 address 'dhcp'
-# set interfaces ethernet eth0 description 'WAN'
-set interfaces ethernet eth0 hw-id 'e4:3a:6e:5f:33:9c'
+# Create interfaces for bond
 
-set interfaces ethernet eth1 address '10.1.0.1/24'
-set interfaces ethernet eth1 description 'LAN'
-set interfaces ethernet eth1 hw-id 'e4:3a:6e:5f:33:9d'
-set interfaces ethernet eth1 vif 10 address '10.1.1.1/24'
-set interfaces ethernet eth1 vif 10 description 'SERVERS'
-set interfaces ethernet eth1 vif 20 address '10.1.2.1/24'
-set interfaces ethernet eth1 vif 20 description 'TRUSTED'
-set interfaces ethernet eth1 vif 30 address '192.168.2.1/24'
-set interfaces ethernet eth1 vif 30 description 'GUEST'
-set interfaces ethernet eth1 vif 40 address '10.1.3.1/24'
-set interfaces ethernet eth1 vif 40 description 'IOT'
-set interfaces ethernet eth1 vif 50 address '10.1.4.1/24'
-set interfaces ethernet eth1 vif 50 description 'VIDEO'
+set interfaces ethernet eth0 hw-id '00:01:c0:21:33:f2'
+set interfaces ethernet eth1 hw-id '00:01:c0:20:71:00'
+set interfaces ethernet eth2 hw-id '00:01:c0:21:33:f1'
+set interfaces ethernet eth3 hw-id '00:01:c0:20:71:01'
 
-set interfaces ethernet eth2 hw-id 'e4:3a:6e:5f:33:9e'
-set interfaces ethernet eth3 hw-id 'e4:3a:6e:5f:33:9f'
-set interfaces ethernet eth4 hw-id 'e4:3a:6e:5f:33:a0'
+# Create Bonded Interface
 
-set interfaces ethernet eth5 hw-id 'e4:3a:6e:5f:33:a1'
-set interfaces ethernet eth5 description 'WAN'
-set interfaces ethernet eth5 address 'dhcp'
+set interfaces bonding bond0 address '10.9.18.1/24'
+set interfaces bonding bond0 description 'MGMT'
+set interfaces bonding bond0 member interface eth0
+set interfaces bonding bond0 member interface eth1
+set interfaces bonding bond0 member interface eth2
+set interfaces bonding bond0 member interface eth3
 
-set interfaces wireguard wg01 address '10.0.11.1/24'
+## Created VIFs on Bond
+set interfaces bonding bond0 vif 5 address '10.5.0.1/16'
+set interfaces bonding bond0 vif 5 description 'RESCUE'
+
+set interfaces bonding bond0 vif 10 address '10.10.0.1/16'
+set interfaces bonding bond0 vif 10 description 'WIRED'
+
+set interfaces bonding bond0 vif 20 address '10.20.0.1/16'
+set interfaces bonding bond0 vif 20 description 'SERVERS'
+
+set interfaces bonding bond0 vif 30 address '10.30.0.1/16'
+set interfaces bonding bond0 vif 30 description 'WIRELESS'
+
+set interfaces bonding bond0 vif 35 address '10.35.0.1/16'
+set interfaces bonding bond0 vif 35 description 'GUEST'
+
+set interfaces bonding bond0 vif 40 address '10.40.0.1/16'
+set interfaces bonding bond0 vif 40 description 'IOT'
+
+set interfaces bonding bond0 vif 50 address '10.50.0.1/16'
+set interfaces bonding bond0 vif 5 description 'VIDEO'
+
+set interfaces bonding bond0 vif 4000 address 'dhcp'
+set interfaces bonding bond0 vif 4000 address 'dhcpv6'
+set interfaces bonding bond0 vif 4000 description 'WAN'
+set interfaces bonding bond0 vif 4000 dhcpv6-options pd 0 interface bond0 sla-id 0
+set interfaces bonding bond0 vif 4000 dhcpv6-options pd 0 interface bond0.10 sla-id 1
+set interfaces bonding bond0 vif 4000 dhcpv6-options pd 0 interface bond0.20 sla-id 2
+set interfaces bonding bond0 vif 4000 dhcpv6-options pd 0 interface bond0.30 sla-id 3
+set interfaces bonding bond0 vif 4000 dhcpv6-options pd 0 length 60
+set interfaces bonding bond0 vif 4000 ipv6 address autoconf
+
+set interfaces wireguard wg01 address '10.11.0.1/16'
 set interfaces wireguard wg01 description 'WIREGUARD'
-set interfaces wireguard wg01 peer ipad-bernd allowed-ips '10.0.11.4/32'
-set interfaces wireguard wg01 peer ipad-bernd persistent-keepalive '15'
-set interfaces wireguard wg01 peer ipad-bernd public-key 'EYuUoDoCIcSYbnjNyPSsZUcz4Yp032AHWflYlH2V1EM='
-set interfaces wireguard wg01 peer iphone-bernd allowed-ips '10.0.11.2/32'
-set interfaces wireguard wg01 peer iphone-bernd persistent-keepalive '15'
-set interfaces wireguard wg01 peer iphone-bernd public-key '608IT2YRPTCabLMGnpYfIWhgDnEpLV5IIpYi5J7m3As='
-set interfaces wireguard wg01 peer macbook-bernd allowed-ips '10.0.11.3/32'
-set interfaces wireguard wg01 peer macbook-bernd persistent-keepalive '15'
-set interfaces wireguard wg01 peer macbook-bernd public-key 'iWKTc88fDBhqlsAgoCXYvkBdf9wG4YBZ8XTg/+0Ln2I='
+set interfaces wireguard wg01 peer iphone-ryan allowed-ips '10.11.0.2/32'
+set interfaces wireguard wg01 peer iphone-ryan persistent-keepalive '15'
+set interfaces wireguard wg01 peer iphone-ryan public-key 'bi5n7JHhg3SNZ/BHOS7F99ou39NKt8L2RmCNzKLiOw4=='
+set interfaces wireguard wg01 peer rymac allowed-ips '10.0.11.2/32'
+set interfaces wireguard wg01 peer rymac persistent-keepalive '15'
+set interfaces wireguard wg01 peer rymac public-key '/8Kd5E9GpwwY+KdR5dhOZuR+TDqMcOgXAOm9k7p5zU0='
 set interfaces wireguard wg01 port '51820'
 set interfaces wireguard wg01 private-key "${SECRET_WIREGUARD_PRIVATE_KEY}"
