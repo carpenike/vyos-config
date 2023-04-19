0468f6652a5028260356b7aaeabe4a0b26e0f609
Ryan Holt <ryan@ryanholt.net>
2023-04-19 16:32:14 -0400

    fix IP addresses of remote-as nodes

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -3,18 +3,18 @@
 # BGP configuration
 set protocols bgp neighbor 10.20.0.20 address-family ipv4-unicast
 set protocols bgp neighbor 10.20.0.20 description 'cp-0'
-set protocols bgp neighbor 10.20.1.20 remote-as '64512'
-set protocols bgp neighbor 10.20.0.21 address-family ipv4-unicast
-set protocols bgp neighbor 10.20.0.21 description 'node-0'
-set protocols bgp neighbor 10.20.1.21 remote-as '64512'
-set protocols bgp neighbor 10.20.0.22 address-family ipv4-unicast
-set protocols bgp neighbor 10.20.0.22 description 'node-1'
-set protocols bgp neighbor 10.20.1.22 remote-as '64512'
-set protocols bgp neighbor 10.20.0.23 address-family ipv4-unicast
-set protocols bgp neighbor 10.20.0.23 description 'node-2'
-set protocols bgp neighbor 10.20.1.23 remote-as '64512'
-set protocols bgp neighbor 10.20.0.24 address-family ipv4-unicast
-set protocols bgp neighbor 10.20.0.24 description 'node-3'
-set protocols bgp neighbor 10.20.1.24 remote-as '64512'
+set protocols bgp neighbor 10.20.0.20 remote-as '64512'
+set protocols bgp neighbor 10.20.10.21 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.10.21 description 'node-0'
+set protocols bgp neighbor 10.20.10.21 remote-as '64512'
+set protocols bgp neighbor 10.20.10.22 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.10.22 description 'node-1'
+set protocols bgp neighbor 10.20.10.22 remote-as '64512'
+set protocols bgp neighbor 10.20.10.23 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.10.23 description 'node-2'
+set protocols bgp neighbor 10.20.10.23 remote-as '64512'
+set protocols bgp neighbor 10.20.10.24 address-family ipv4-unicast
+set protocols bgp neighbor 10.20.10.24 description 'node-3'
+set protocols bgp neighbor 10.20.10.24 remote-as '64512'
 set protocols bgp parameters router-id '10.20.0.1'
 set protocols bgp system-as '64512'
