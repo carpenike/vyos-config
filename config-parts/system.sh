828c3726e6334978106efbd3a2820f4e8272b263
Ryan Holt <ryan@ryanholt.net>
2023-05-04 15:41:36 -0400

    move system to dnsdist for resolution

--- config-parts/system.sh
+++ config-parts/system.sh
@@ -6,7 +6,7 @@ set system host-name 'fw'
 set system login user vyos authentication public-keys personal key 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOslNYCKlAhgO9vxUVt4Vq0diz35JD0f6Vtdh2zfZwyb+SI/TPC+U06TPsxS++KN+HHkQvNBcqpQ6a8qNsYsVJA='
 set system login user vyos authentication public-keys personal type 'ecdsa-sha2-nistp256'
 
-set system name-server '1.1.1.1'
+set system name-server '10.6.0.4'
 
 set system sysctl parameter kernel.pty.max value '24000'
 
