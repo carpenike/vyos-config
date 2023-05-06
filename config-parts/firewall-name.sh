b2157f0d4437b6298725e16abaf3c336017dc53a
Ryan Holt <ryan@ryanholt.net>
2023-05-06 18:14:42 -0400

    forgot to define wan zone

--- config-parts/firewall-name.sh
+++ config-parts/firewall-name.sh
@@ -17,6 +17,10 @@ set firewall ipv6-name wan-local default-action 'drop'
 set firewall ipv6-name wan-local description 'From WAN to LOCAL'
 set firewall ipv6-name wan-local enable-default-log
 set firewall ipv6-name wan-local rule 1 action 'accept'
-set firewall ipv6-name wan-local rule 1 description 'Rule: accept_wireguard'
-set firewall ipv6-name wan-local rule 1 destination port '51820'
-set firewall ipv6-name wan-local rule 1 protocol 'udp'
+set firewall ipv6-name wan-local rule 1 description 'Rule: accept_ipv6-icmp'
+set firewall ipv6-name wan-local rule 1 protocol 'ipv6-icmp'
+
+set firewall ipv6-name wan-local rule 2 action 'accept'
+set firewall ipv6-name wan-local rule 2 description 'Rule: accept_wireguard'
+set firewall ipv6-name wan-local rule 2 destination port '51820'
+set firewall ipv6-name wan-local rule 2 protocol 'udp'
--- config-parts/firewall-zone.sh
+++ config-parts/firewall-zone.sh
@@ -12,3 +12,14 @@ set firewall zone local description 'Local router zone'
 # set firewall zone local from video firewall name 'video-local'
 set firewall zone local from wan firewall name 'wan-local'
 set firewall zone local local-zone
+
+# WAN
+# set firewall zone wan from guest firewall name 'guest-wan'
+# set firewall zone wan from iot firewall name 'iot-wan'
+# set firewall zone wan from lan firewall name 'lan-wan'
+# set firewall zone wan from local firewall name 'local-wan'
+# set firewall zone wan from servers firewall name 'servers-wan'
+# set firewall zone wan from services firewall name 'services-wan'
+# set firewall zone wan from trusted firewall name 'trusted-wan'
+# set firewall zone wan from video firewall name 'video-wan'
+set firewall zone wan interface 'bond0.4000'
