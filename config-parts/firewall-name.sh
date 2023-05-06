6fe8fddeacc37230295fcfd3aa4c9f7d1cc3cde6
Ryan Holt <ryan@ryanholt.net>
2023-05-06 19:31:40 -0400

    accept icmp from wireless to wan and re-order

--- config-parts/firewall-name.sh
+++ config-parts/firewall-name.sh
@@ -40,44 +40,48 @@ set firewall name wireless-wan default-action 'drop'
 set firewall name wireless-wan description 'From WIRELESS to WAN'
 set firewall name wireless-wan enable-default-log
 set firewall name wireless-wan rule 1 action 'accept'
-set firewall name wireless-wan rule 1 description 'Rule: accept_http'
-set firewall name wireless-wan rule 1 destination port 'http'
-set firewall name wireless-wan rule 1 protocol 'tcp'
+set firewall name wireless-wan rule 1 description 'Rule: accept_icmp'
+set firewall name wireless-wan rule 1 protocol 'icmp'
 
 set firewall name wireless-wan rule 2 action 'accept'
-set firewall name wireless-wan rule 2 description 'Rule: accept_https'
-set firewall name wireless-wan rule 2 destination port 'https'
+set firewall name wireless-wan rule 2 description 'Rule: accept_http'
+set firewall name wireless-wan rule 2 destination port 'http'
 set firewall name wireless-wan rule 2 protocol 'tcp'
 
 set firewall name wireless-wan rule 3 action 'accept'
-set firewall name wireless-wan rule 3 description 'Rule: accept_wifi_calling'
-set firewall name wireless-wan rule 3 destination port '500,4500'
-set firewall name wireless-wan rule 3 source group domain-group verizon_wifi_calling
-set firewall name wireless-wan rule 3 protocol 'udp'
+set firewall name wireless-wan rule 3 description 'Rule: accept_https'
+set firewall name wireless-wan rule 3 destination port 'https'
+set firewall name wireless-wan rule 3 protocol 'tcp'
 
 set firewall name wireless-wan rule 4 action 'accept'
-set firewall name wireless-wan rule 4 description 'Rule: accept_amcrest_clients_to_amcrest_cloud_udp'
-set firewall name wireless-wan rule 4 destination port '8000-8815'
-set firewall name wireless-wan rule 4 source group domain-group amcrest_clients
+set firewall name wireless-wan rule 4 description 'Rule: accept_wifi_calling'
+set firewall name wireless-wan rule 4 destination port '500,4500'
+set firewall name wireless-wan rule 4 source group domain-group verizon_wifi_calling
 set firewall name wireless-wan rule 4 protocol 'udp'
 
 set firewall name wireless-wan rule 5 action 'accept'
-set firewall name wireless-wan rule 5 description 'Rule: accept_smart_link_from_flex_clients_tcp'
-set firewall name wireless-wan rule 5 destination port '4992,4994'
-set firewall name wireless-wan rule 5 source group domain-group flex_clients
-set firewall name wireless-wan rule 5 protocol 'tcp'
+set firewall name wireless-wan rule 5 description 'Rule: accept_amcrest_clients_to_amcrest_cloud_udp'
+set firewall name wireless-wan rule 5 destination port '8000-8815'
+set firewall name wireless-wan rule 5 source group domain-group amcrest_clients
+set firewall name wireless-wan rule 5 protocol 'udp'
 
 set firewall name wireless-wan rule 6 action 'accept'
-set firewall name wireless-wan rule 6 description 'Rule: accept_smart_link_from_flex_clients_udp'
-set firewall name wireless-wan rule 6 destination port '4991,4992,4993'
+set firewall name wireless-wan rule 6 description 'Rule: accept_smart_link_from_flex_clients_tcp'
+set firewall name wireless-wan rule 6 destination port '4992,4994'
 set firewall name wireless-wan rule 6 source group domain-group flex_clients
-set firewall name wireless-wan rule 6 protocol 'udp'
+set firewall name wireless-wan rule 6 protocol 'tcp'
 
 set firewall name wireless-wan rule 7 action 'accept'
-set firewall name wireless-wan rule 7 description 'Rule: accept_plex_from_plex_clients_to_public'
-set firewall name wireless-wan rule 7 destination port '32400'
-set firewall name wireless-wan rule 7 source group domain-group plex_clients
-set firewall name wireless-wan rule 7 protocol 'tcp'
+set firewall name wireless-wan rule 7 description 'Rule: accept_smart_link_from_flex_clients_udp'
+set firewall name wireless-wan rule 7 destination port '4991,4992,4993'
+set firewall name wireless-wan rule 7 source group domain-group flex_clients
+set firewall name wireless-wan rule 7 protocol 'udp'
+
+set firewall name wireless-wan rule 8 action 'accept'
+set firewall name wireless-wan rule 8 description 'Rule: accept_plex_from_plex_clients_to_public'
+set firewall name wireless-wan rule 8 destination port '32400'
+set firewall name wireless-wan rule 8 source group domain-group plex_clients
+set firewall name wireless-wan rule 8 protocol 'tcp'
 
 # FROM LOCAL TO WIRELESS
 set firewall name local-wireless default-action 'drop'
